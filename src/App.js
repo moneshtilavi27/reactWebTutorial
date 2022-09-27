ec326f41db0433ddead0c51cba329f9c21a3f1f0
MoneshTilavi <moneshtilavi22@gmail.com>
2022-09-27 17:05:18 +0530

    make uses a props, events handling, states

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Component } from 'react';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shares/Dishes';
 
 class App extends Component{
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state ={
+      dishes: DISHES
+    };
+  }
+
   render(){
     return(
-      <div >
+      <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Ristorant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={ this.state.dishes} />
       </div>
     )
   }
